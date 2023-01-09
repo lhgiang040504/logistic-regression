0d1d9667639caa22fb6e4b04cc42209055eb8293
Giang Rom <giang@gmail.com>
2023-01-09 16:41:12 +0700

    plot the data after classificate

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,5 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("data_classification.csv", header=None)
-print(data)
+df = pd.read_csv("data_classification.csv", header=None)
+
+x_true = []
+y_true = []
+x_false = []
+y_false = []
+for item in df.values:
+    if item[-1] == 1:
+        x_true.append(item[0])
+        y_true.append(item[1])
+    else:
+        x_false.append(item[0])
+        y_false.append(item[1])
+plt.plot(x_true, y_true, 'o', c='blue')
+plt.plot(x_false, y_false, 'o', c='red')
+plt.show()
---
+++ test.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv("data_classification.csv", header=None)
+print(df.columns)
