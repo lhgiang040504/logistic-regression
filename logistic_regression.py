3ff0976e718b0b4658917d85081609218b2d3a1e
Giang Rom <giang@gmail.com>
2023-01-12 14:25:31 +0700

    comback

--- logistic_regression.py
+++ logistic_regression.py
@@ -15,6 +15,32 @@ for item in df.values:
     else:
         x_false.append(item[0])
         y_false.append(item[1])
+
 plt.plot(x_true, y_true, 'o', c='blue')
 plt.plot(x_false, y_false, 'o', c='red')
 plt.show()
+
+def  sigmoid(z):
+    return 1.0/(1 + np.exp(-1))
+
+def filter_data(pred):
+    if pred >= 0.5:
+        return 1
+    else:
+        return 0
+def predict(features, weights):
+    pred = np.doc(features, weights)
+    return sigmoid(pred)
+
+def CE(features, weights, labels):
+    '''matrics
+    features: 100x3 (replace label with bias)
+    weight: 1x3 (use the .T of numpy)
+    label: 100x1
+    '''
+    prediction = predict(features, weights)
+    n = len(df)
+    cost_class1 = -labels*np.log(prediction)
+    cost_class2 = -(1-labels)*np.log(1 - prediction)
+    cost= cost_class1 + cost_class2
+    return cost.sum()/n
