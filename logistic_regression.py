f9f7e30b279c44ca9c4b9c0a63223f838c2239de
Giang Rom <giang@gmail.com>
2023-02-10 10:09:56 +0700

    show_problem

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,19 +7,28 @@ df = pd.read_csv("data_classification.csv", header=None)
 
 train, test = train_test_split(df, test_size = 0.3, random_state=40)
 
-x_train = df.drop(columns = df.columns[-1])
-y_train = df[df.columns[-1]]
+features_train = train.drop(columns = df.columns[-1])
+labels_train = train[df.columns[-1]]
 
-x_test = df.drop(columns = df.columns[-1])
-y_test = df[df.columns[-1]]
+features_test = test.drop(columns = df.columns[-1])
+labels_test = test[df.columns[-1]]
 
+features_train['bias'] = pd.DataFrame(pd.Series(1,index = features_train.index))
+features_test['bias'] = pd.DataFrame(pd.Series(1,index = features_train.index))
+
+labels_train = np.array(labels_train)
+print(labels_train)
+
+weights = np.array([[1],
+[1],
+[1]])
 
 '''
 x_true = []
 y_true = []
 x_false = []
 y_false = []
-##1x100
+#1x100
 
 for item in df.values:
     if item[-1] == 1:
@@ -36,10 +45,14 @@ plt.show()
 '''
 
 def  sigmoid(z):
-    return 1.0/(1 + np.exp(-1))
+    '''aglorthm
+    pred = weights * feature
+    z    = w1*x1 + w2*x2 + 1*1(bias)
+    '''
+    return 1.0/(1 + np.exp(-z))
 
 def filter_data(pred):
-    if pred >= 0.8:
+    if pred >= 0.9999:
         return 1
     else:
         return 0
@@ -50,20 +63,20 @@ def predict(features, weights):
 def CE(features, weights, labels):
     '''matrics
     features: 100x3 (replace label with bias)
-    weight: 1x3 (use the .T of numpy)**
+    weight: 1x3
     label: 100x1
     '''
     prediction = predict(features, weights)
-    n = len(df)
-    cost_class1 = -labels*np.log(prediction)
-    cost_class2 = -(1-labels)*np.log(1 - prediction)
+    n = len(labels)
+    cost_class1 = -labels * prediction
+    cost_class2 = -(1-labels) * (1 - prediction)
     cost= cost_class1 + cost_class2
     return cost.sum()/n
 
 def update_weights(features, weights, labels, learning_rate):
-    '''matrics
+    '''matric
     features: 100x3 (replace label with bias)
-    weight: 1x3 (use the .T of numpy)**
+    weight: 1x3
     label: 100x1
     learning_rate: float
     '''
@@ -85,5 +98,14 @@ def train(features, weights, labels, learning_rate, iter):
             break
     return weights, Cost_history
 
+learning_rate = 0.001
+iter = 1000
 
-weights, cost_history = train(features, weights, labels, 0.01, 100)
+#best_weights, Cost_history = train(features_train, weights, labels_train, learning_rate, iter)
+prediction = predict(features_train, weights)
+n = len(labels_train)
+#cost_class1 = -labels_train * prediction
+#cost_class2 = -(1-labels_train) * (1 - prediction)
+#cost= cost_class1 + cost_class2
+#labels_train = np.array(labels_train)
+
---
+++ show_problem.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data_classification.csv", header=None)
+
+x_true = []
+y_true = []
+x_false = []
+y_false = []
+
+for item in df.values:
+    if item[-1] == 1:
+        x_true.append(item[0])
+        y_true.append(item[1])
+    else:
+        x_false.append(item[0])
+        y_false.append(item[1])
+
+plt.xlabel("SLEEP")
+plt.ylabel("STUDY")
+plt.plot(x_true, y_true, 'o', c='blue')
+plt.plot(x_false, y_false, 'o', c='red')
+plt.show()
