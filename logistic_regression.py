71518da6f5edf575c2f70c39e505d3192b5bef30
Giang Rom <giang@gmail.com>
2023-02-11 15:28:01 +0700

    last version

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,47 +1,41 @@
+'''
+
+* Logistic Regression
+* sourse dataset : https://www.scilab.org/machine-learning-logistic-regression-tutorial
+
+'''
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("data_classification.csv", header=None)
+#read the data file
+df = pd.read_csv('data_classification.csv')
 
+#split to train and test data
 train, test = train_test_split(df, test_size = 0.3, random_state=40)
-
 features_train = train.drop(columns = df.columns[-1])
 labels_train = train[df.columns[-1]]
-
 features_test = test.drop(columns = df.columns[-1])
 labels_test = test[df.columns[-1]]
 
+#add the bias
 features_train['bias'] = pd.DataFrame(pd.Series(1,index = features_train.index))
-features_test['bias'] = pd.DataFrame(pd.Series(1,index = features_train.index))
+features_test['bias'] = pd.DataFrame(pd.Series(1,index = features_test.index))
 
-labels_train = np.array(labels_train)
-print(labels_train)
+#convert to matrix
+features_train = np.array(features_train)
+labels_train = np.array([labels_train])#1x10
+features_test = np.array(features_test)
+labels_test = np.array([labels_test])
 
-weights = np.array([[1],
-[1],
-[1]])
+weights = np.array([[1.], [1.], [1.]])
 
-'''
-x_true = []
-y_true = []
-x_false = []
-y_false = []
-#1x100
-
-for item in df.values:
-    if item[-1] == 1:
-        x_true.append(item[0])
-        y_true.append(item[1])
-    else:
-        x_false.append(item[0])
-        y_false.append(item[1])
-
-
-plt.plot(x_true, y_true, 'o', c='blue')
-plt.plot(x_false, y_false, 'o', c='red')
-plt.show()
+'''matrixs push into functions
+features: 100x3 (replace label with bias)
+weight: 3x1
+label: 100x1
 '''
 
 def  sigmoid(z):
@@ -51,61 +45,58 @@ def  sigmoid(z):
     '''
     return 1.0/(1 + np.exp(-z))
 
-def filter_data(pred):
-    if pred >= 0.9999:
-        return 1
-    else:
-        return 0
+def filter(pred):
+    for item in pred:
+        if item[-1] > 0.9999:
+            item[-1] = 1
+        else:
+            item[-1] = 0
+    return pred
+
 def predict(features, weights):
     pred = np.dot(features, weights)
     return sigmoid(pred)
 
+#cross entropy
 def CE(features, weights, labels):
-    '''matrics
-    features: 100x3 (replace label with bias)
-    weight: 1x3
-    label: 100x1
-    '''
     prediction = predict(features, weights)
     n = len(labels)
-    cost_class1 = -labels * prediction
-    cost_class2 = -(1-labels) * (1 - prediction)
+    cost_class1 = -labels * np.log(prediction)
+    cost_class2 = -(1-labels) * np.log((1 - prediction))
     cost= cost_class1 + cost_class2
     return cost.sum()/n
 
 def update_weights(features, weights, labels, learning_rate):
-    '''matric
-    features: 100x3 (replace label with bias)
-    weight: 1x3
-    label: 100x1
-    learning_rate: float
-    '''
     n = len(labels)
     prediction = predict(features, weights)
-    gradient = np.dot(features, prediction -  labels)
+    gradient = np.dot(features.T, prediction -  labels)
     gradient = gradient/n
     gradient = gradient*learning_rate
     weights -= gradient
     return weights
 
 def train(features, weights, labels, learning_rate, iter):
-    Cost_history = []
+    Cost_history = [0]
     for i in range(iter):
         weights = update_weights(features, weights, labels, learning_rate)
         Cost = CE(features, weights, labels)
         Cost_history.append(Cost)
-        if (Cost_history[-1] - Cost_history[-2] < 0.001):
+        if (Cost_history[-1] - Cost_history[-2] < 0.00001):
             break
     return weights, Cost_history
 
-learning_rate = 0.001
+learning_rate = 0.0001
 iter = 1000
 
-#best_weights, Cost_history = train(features_train, weights, labels_train, learning_rate, iter)
-prediction = predict(features_train, weights)
-n = len(labels_train)
-#cost_class1 = -labels_train * prediction
-#cost_class2 = -(1-labels_train) * (1 - prediction)
-#cost= cost_class1 + cost_class2
-#labels_train = np.array(labels_train)
+best_weights, Cost_history = train(features_train, weights, labels_train.T, learning_rate, iter)
+prediction = predict(features_test, best_weights)
+result = filter(prediction)
 
+labels_test = labels_test.T
+count = 0
+print(result[0][0] - 1)
+for i in range(len(result)):
+    if int(result[i][0]) == labels_test[i][0]:
+        count += 1
+
+print((count / len(result))*100)
